7b4e6749ce0c5b779719a77b9295dbc5d2c153d6
jeffyangcoder <55988379+jeffyangcoder@users.noreply.github.com>
2021-03-09 06:21:51 -0800

    Create 556.cpp

---
+++ 556.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int m = nums.size();
+        int n = nums[0].size();
+        if(m * n !=(long long) r * c) return nums;
+
+        vector<vector<int>> ans(r, vector<int>(c));
+        for(int i = 0;i < m * n ;i++)
+            ans[i / c][i % c] = nums[i / n][i % n]; 
+        
+        return ans;
+    }
+};
